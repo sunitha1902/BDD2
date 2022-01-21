dbe23c98fc6052ab6baf7bde341b75e23055372f
sunitha1902 <sunithabandaru19@gmail.com>
2022-01-21 19:53:50 +0530

    commit

--- FrameworkHybrid/Milestoneframework/src/test/java/uistore/ContactUsPageUI.java
+++
@@ -1,8 +0,0 @@
-package uistore;
-
-import org.openqa.selenium.By;
-
-public class ContactUsPageUI {
-	public static By contactus = By.xpath(".//*[@href='/contact-us']");
-	public static By phoneno = By.xpath(".//*[@href='tel:90 4545 0000']");
-}
